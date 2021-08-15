3ec9afca3d403bdad15e5d923d7be73c6d48372c
YafitSamuel <gyafit642@gmail.com>
2021-08-15 16:51:49 +0300

    commit

---
+++ bank/main.js
@@ -0,0 +1,75 @@
+//  =================Bank Management===========
+
+// const customers = [
+//     { id: 11, Name: "yafit", Phone: "056747364", Email: "abvfjb.com", Balance: 57 },
+//     { id: 12, Name: "bar", Phone: "056467364", Email: "gbvfsxgjb.com", Balance: 9000 },
+//     { id: 13, Name: "nir", Phone: "056237364", Email: "cbfsvfjb.com", Balance: 67980 },
+//     { id: 14, Name: "tali", Phone: "057847365", Email: "rgfb.com", Balance: -5753 },
+
+// ]
+// ==================================================================
+// function naAddClientme(customers) {
+//     customers.push({id:15, Name:"ayelet",Phone:"054759479", Email:"rhfgvhfgrhgdj.com", Balance:-5113});
+//     return customers
+// }
+// console.log(naAddClientme(customers));
+// ==================================================================
+// function GetClientById(customers,id){
+//     for (let i = 0; i < customers.length; i++) {
+//         if (id == customers[i].id){
+//             return customers[i]
+//         }
+//     }
+// }
+// console.log(GetClientById(customers,11));
+// ==================================================================
+
+// function DeleteClientById(customers,id){
+//     for(let i=0; i<customers.length;i++){
+//         if(id==customers[i].id){
+//             customers.splice(i,1)
+//             return customers
+//         }
+//     }
+// }
+// console.log(DeleteClientById(customers,11));
+// ==================================================================
+// function WithdrawMoneyFindById(customers,id,less) {
+//     for(let i=0;i<customers.length;i++){
+//         if(id==customers[i].id){
+//             customers[i].Balance-=less
+//             return customers[i]
+//         }
+//     }
+// }
+// console.log(WithdrawMoneyFindById(customers,11,200));
+// ==================================================================
+// function DepositMoneyFindById(customers,id,add){
+//     for(let i=0;i<customers.length;i++){
+//         if(id==customers[i].id){
+//             customers[i].Balance+=add
+//             return customers[i]
+//         }
+//     }
+// }
+// console.log(DepositMoneyFindById(customers,11,1000));
+// ==================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
